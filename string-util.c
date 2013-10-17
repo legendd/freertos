200666976f0ca65897bff94fc44a703f48cf9e57
legendd <legendshiao@hotmail.com>
2013-10-17 16:55:39 +0800

    put function strcmp() to string-util.c

--- string-util.c
+++ string-util.c
@@ -67,3 +67,9 @@ char *strncpy(char *dest, const char *src, size_t n)
 	while (n-- && (*d++ = *s++));
 	return dest;
 }
+int strcmp(const char* s1, const char* s2)
+{
+    while(*s1 && (*s1==*s2))
+        s1++,s2++;
+    return *(const unsigned char*)s1-*(const unsigned char*)s2;
+}
